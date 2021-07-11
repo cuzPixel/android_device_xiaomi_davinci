b2dceed877c1ac504b1c71458eb016729071dac2
maytinhdibo <iammaytinhdibo@gmail.com>
2021-07-11 17:50:01 +0700

    davinci: Add new pocket mode
    -: By @maytinhdibo
    -: update event for fingerprint wake up screen
    -: remove duplicate event turn on service
    -: Made it more battery friendly
    -: Reworked pocket utils

--- device.mk
+++ device.mk
@@ -69,6 +69,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# Pocket Mode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
+
 # Power
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
